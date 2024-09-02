7f0f8a452b281d4253d9262f5cdc51e96f73fe01
Julie Lee <chiatunglee@systex.com>
2024-09-02 15:33:38 +0800

    create e2e testing file

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
     path: 'unit-test-example',
     loadChildren: () =>
       import('./unit-test-example/unit-test-example.module').then(
