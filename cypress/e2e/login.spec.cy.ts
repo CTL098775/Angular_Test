b837fb4ce61b34fda805b3a039407cb02d577a30
Julie Lee <chiatunglee@systex.com>
2024-09-03 16:51:39 +0800

    add login e2e test

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -4,10 +4,64 @@ describe('Login Page Tests', () => {
     cy.visit('/login');
   });
 
-  it('正確顯示登入並登入', () => {
+  it('應正確顯示登入表單', () => {
     // 驗證登錄畫面顯示表單
     cy.get('input[formControlName="username"]').should('be.visible');
     cy.get('input[formControlName="password"]').should('be.visible');
     cy.get('button[type="submit"]').should('be.visible');
+  });
+
+  it('應顯示未輸入欄位的驗證', () => {
+    // 點擊提交按鈕(不輸入內容)
+    cy.get('button[type="submit"]').click();
+
+    // 驗證表單顯示錯誤消息
+    cy.get('input[formControlName="username"]').should(
+      'have.class',
+      'ng-invalid',
+    );
+    cy.get('input[formControlName="password"]').should(
+      'have.class',
+      'ng-invalid',
+    );
+
+    // 驗證顯示錯誤訊息
+    cy.contains('此欄位必須輸入').should('be.visible');
+  });
+
+  it('應使用有效帳密並成功登入', () => {
+    // 填寫有效的帳號密碼
+    cy.get('input[formControlName="username"]').type('testuser');
+    cy.get('input[formControlName="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+
+    // 驗證成功登錄後到主頁
+    cy.url().should('include', '/home/unit-test-example');
+    cy.contains('Welcome').should('be.visible');
+  });
+
+  it('應顯示登入無效錯誤訊息', () => {
+    // 填寫無效的帳號密碼
+    cy.get('input[formControlName="username"]').type('invaliduser');
+    cy.get('input[formControlName="password"]').type('wrongpassword');
+    cy.get('button[type="submit"]').click();
+
+    // 驗證顯示 error message
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('帳號密碼輸入錯誤');
+    });
+  });
+
+  it('應該可成功登出', () => {
+    // 先登錄
+    cy.get('input[formControlName="username"]').type('testuser');
+    cy.get('input[formControlName="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+
+    // 點登出
+    cy.get('.logout-button').click();
+
+    // 驗證回到登錄頁面
+    cy.url().should('include', '/login'); // 驗證是否重新回到登錄頁面
   });
 });
